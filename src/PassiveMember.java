257d662accf2a0d440d7594c81b36e614b7cb41c
MBaun6 <mlbaun5@gmail.com>
2026-05-06 09:53:24 +0200

    active og passive opdateret

---
+++ src/PassiveMember.java
@@ -0,0 +1,10 @@
+public class PassiveMember {
+    double subscription;
+    public PassiveMember(String name, int age, boolean status, double subscription) {
+        super();
+        this.subscription=subscription;
+    }
+    public double getSubscription(){
+        return subscription;
+    }
+}
