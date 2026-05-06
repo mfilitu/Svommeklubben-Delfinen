beec1bf98a1bdb91001f54c95a070d4612717f31
Mateusz Andrzej Filipowski <70143907+mfilitu@users.noreply.github.com>
2026-05-06 09:48:05 +0200

    Member class

---
+++ src/Member.java
@@ -0,0 +1,39 @@
+public class Member {
+    private final String name;
+    private final int age;
+    private boolean status = true;
+
+    public Member(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public boolean isSeniorMember(){
+        return age >= 18;
+    }
+
+    public boolean isJuniorMember(){
+        return age < 18;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean getStatus(){
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Navn: %s\nAlder: %s\nStatus: %s", name, age, status);
+    }
+}
