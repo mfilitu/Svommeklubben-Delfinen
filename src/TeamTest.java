593678b86c0e0d6fc40af3ac363908200ac89efa
PhilipMather04 <philip@mather.dk>
2026-05-07 12:09:32 +0200

    Update TeamTest.java

--- src/TeamTest.java
+++ src/TeamTest.java
@@ -6,10 +6,18 @@ class TeamTest {
 
     @Test
         public void testAddSeniorMember() {
+
+            //Arrange
+
             Team team = new Team("Delfinen");
             ActiveMember senior = new ActiveMember("Philip", 22);
 
+
+            //Act
+
             team.addToTeam(senior);
+
+            //Assert
 
             String teamAsString = team.toString();
             assertTrue(teamAsString.contains("Philip"));
