20c8893054f36699996f4b29b55c423bde5511d4
Mateusz Andrzej Filipowski <70143907+mfilitu@users.noreply.github.com>
2026-05-11 10:18:08 +0200

    Payment status done

--- src/Code/App.java
+++ src/Code/App.java
@@ -6,6 +6,7 @@ public class App {
     public static void main(String[] args) throws InvalidAgeException {
 
         Register register = new Register();
+        PaymentStatus paymentStatus = new PaymentStatus(register);
         Boolean running = true;
 
         Scanner scanner = new Scanner(System.in);
@@ -37,6 +38,7 @@ public class App {
                             createMember(age, register, name);
 
                             System.out.println(name + " er blevet oprettet som medlem!");
+
                             break;
                         case 2:
                             //aktive
@@ -57,23 +59,76 @@ public class App {
                     System.out.println("\nListe af alle medlemmer:");
                     System.out.println(register);
                     break;
+                case 3:
+                    System.out.println();
+                    System.out.println("--------------------------------------");
+                    System.out.println("1. Vis forventet kontigent");
+                    System.out.println("2. Vis missing payment");
+                    //System.out.println("3. ");
+                    System.out.println("--------------------------------------");
+                    int payment_menu = scanner.nextInt();
+                    switch (payment_menu){
+                        case 1:
+                            System.out.println(paymentStatus.getRevenue());
+                            break;
+                        case 2:
+                            System.out.println(paymentStatus.getMissingPayment());
+                            break;
+                    }
+                    break;
+
             }
         }
     }
 
     private static void createMember(int age, Register register, String name) {
+        Scanner scanner = new Scanner(System.in);
+
         // Note: 1-17 = junior. 18-99 = senior. 0 og 100+ = InvalidAgeException
         if (age >= 18 && age <= 100) {
-            register.addMember(new SeniorMember(name, age));
+            Member seniorMember = new SeniorMember(name, age);
+            register.addMember(seniorMember);
+            System.out.println("Betal med det samme? 1 = ja, 2 = nej.");
+            int pay = scanner.nextInt();
+            switch (pay){
+                case 1:
+                    seniorMember.pay();
+                    break;
+                case 2:
+                    break;
+            }
         } else if (age > 0 && age < 18) {
-            register.addMember(new JuniorMember(name, age));
+            Member juniorMember = new JuniorMember(name, age);
+            register.addMember(juniorMember);
+            System.out.println("Betal med det samme? 1 = ja, 2 = nej.");
+            int pay = scanner.nextInt();
+            switch (pay){
+                case 1:
+                    juniorMember.pay();
+                    break;
+                case 2:
+                    break;
+            }
         } else {
             throw new InvalidAgeException("Personen er for ung/gammel");
         }
+
     }
 
     private static void createPassiveMember(int age, Register register, String name) {
-        register.addMember(new PassiveMember(name, age));
+        Member member = new PassiveMember(name, age);
+        register.addMember(member);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Betal med det samme? 1 = ja, 2 = nej.");
+        int pay = scanner.nextInt();
+        switch (pay){
+            case 1:
+                member.pay();
+                break;
+            case 2:
+                break;
+        }
     }
 
     private static void printMenu() {
@@ -82,7 +137,7 @@ public class App {
         System.out.println("Menu:");
         System.out.println("1. Opret medlem");
         System.out.println("2. Vis liste af alle medlemer");
-        System.out.println("3. ");
+        System.out.println("3. Kontigent overblik");
         System.out.println("--------------------------------------");
     }
 }
--- src/Code/Main.java
+++ src/Code/Main.java
@@ -4,22 +4,27 @@ package Code;
 public class Main {
     public static void main(String[] args) {
         Register register = new Register();
+        PaymentStatus paymentStatus = new PaymentStatus(register);
         ActiveMember bo = new SeniorMember("Bo", 17);
         ActiveMember bob = new JuniorMember("Bob", 1);
         PassiveMember bobby = new PassiveMember("Bobby", 55);
 
 
-
+        register.addMember(bo);
         register.addMember(bob);
         register.addMember(bobby);
+
+        bo.pay();
+        System.out.println(bo.isPaid());
+        bob.pay();
+        //bobby.pay();
         System.out.println(bo.getFee());
 
+        System.out.println(paymentStatus.getRevenue());
+        System.out.println(paymentStatus.getMissingPayment());
+        paymentStatus.savePaidToFile();
+        paymentStatus.saveUnpaidToFile();
 
-        Result bo_res = new Result(bo, SwimmingDiscipline.BackCrawl, 1000);
-
-        ResultList list = new ResultList();
-
-        //list.addResult
 
 
     }
