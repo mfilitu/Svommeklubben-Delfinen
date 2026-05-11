e1d99dbb8966703e174deb2513b063bdd210e0d3
PhilipMather04 <philip@mather.dk>
2026-05-11 09:48:44 +0200

    top5

--- src/Code/Main.java
+++ src/Code/Main.java
@@ -1,25 +1,37 @@
 package Code;
 
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Register register = new Register();
+       Register register = new Register();
         ActiveMember bo = new SeniorMember("Bo", 17);
         ActiveMember bob = new JuniorMember("Bob", 1);
         PassiveMember bobby = new PassiveMember("Bobby", 55);
 
 
 
-        register.addMember(bob);
-        register.addMember(bobby);
-        System.out.println(bo.getFee());
-
 
         Result bo_res = new Result(bo, SwimmingDiscipline.BackCrawl, 1000);
 
+
+
         ResultList list = new ResultList();
 
-        //list.addResult
+        ArrayList<ResultList> resultLists = new ArrayList<>();
+        list.addResult(new Result(bo, SwimmingDiscipline.BackCrawl, 1000));
+        list.addResult(new Result(bo, SwimmingDiscipline.BackCrawl, 900));
+        list.addResult(new Result(bo, SwimmingDiscipline.BackCrawl, 1100));
+
+
+        for (Result resultLists1 : list.getTopFive(SwimmingDiscipline.BackCrawl)) {
+            System.out.println(resultLists1);
+        }
+
+
+
+
 
 
     }
--- src/Code/ResultList.java
+++ src/Code/ResultList.java
@@ -1,12 +1,31 @@
 package Code;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ResultList {
     List<Result> results = new ArrayList<>();
 
     public ResultList(){
+    }
 
+    public void addResult(Result result){
+        results.add(result);
+    }
+    public List<Result> getAllResults(){
+        return results;
+    }
+
+    public List<Result> getTopFive(SwimmingDiscipline discipline){
+        return results.stream()
+                .filter(r -> r.getDiscipline() == discipline)
+                .sorted(Comparator.comparingInt(Result::getTime))
+                .limit(5)
+                .collect(Collectors.toList());
     }
 }
+
+
+
